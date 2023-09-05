c503715e8af648de5f277db82c3bf16f04644dad
Maicon Gambini <maikinho38@gmail.com>
2023-09-05 17:49:45 -0300

    commit DIO

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+const limit = 5
+let offset = 0
+const maxRecords = 15
+
+
+function loadPokemonItens (offset,limit) {
+  function convertPokemonToLi(pokemon) {
+    return `
+  <li class="pokemon ${pokemon.type}">
+  <span class="number">#${pokemon.number}</span>
+  <span class="name">${pokemon.name}</span>
+  
+  <div class="detail">
+    <ol class="types">
+    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+    </ol>
+  
+    <img src="${pokemon.photo}" 
+    alt="${pokemon.name}">
+  
+  </div>
+  
+  </li>
+  `;
+  }
+
+
+
+  pokeApi.getPokemons(offset,limit).then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+
+
+  })
+}
+
+loadPokemonItens(offset,limit)
+
+loadMoreButton.addEventListener('click', () => {
+  offset += limit
+
+  const qtdRecordNextPage = offset + limit
+if (qtdRecordNextPage >= maxRecords){
+  const newLimit = maxRecords - offset
+  loadPokemonItens(offset,newLimit)
+loadMoreButton.parentElement.removeChild(loadMoreButton)
+
+} else {
+  loadPokemonItens(offset,limit)
+}
+})
+
